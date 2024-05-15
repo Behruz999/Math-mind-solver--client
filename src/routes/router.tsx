0f30af3de4acc6f6394818e732f49b9594d101e2
DarksideEvil <sinister58450@gmail.com>
2024-05-15 18:35:02 +0500

    last one

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,16 +3,18 @@ import { Home } from "../views/home";
 import { Second } from "../views/second";
 import { Example } from "../views/example";
 import { Summary } from "../views/summary";
+import ScrollToTop from "../components/loader";
 
 export const MainRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="second" element={<Second />} />
-            <Route path="example" element={<Example />} />
-            <Route path="summary" element={<Summary />} />
-        </Routes>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="second" element={<Second />} />
+        <Route path="example" element={<Example />} />
+        <Route path="summary" element={<Summary />} />
+      </Routes>
     </>
   )
 }
